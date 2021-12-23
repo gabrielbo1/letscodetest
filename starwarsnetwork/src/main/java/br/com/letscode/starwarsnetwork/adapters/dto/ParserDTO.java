5a622a9c25a01984a1b532968e9c27a02f0ea9bc
Gabriel Oliveira <barbosa.olivera1@gmail.com>
2021-12-23 11:31:28 -0300

    Codacy issues.

--- starwarsnetwork/src/main/java/br/com/letscode/starwarsnetwork/adapters/dto/ParserDTO.java
+++ starwarsnetwork/src/main/java/br/com/letscode/starwarsnetwork/adapters/dto/ParserDTO.java
@@ -1,7 +1,5 @@
 package br.com.letscode.starwarsnetwork.adapters.dto;
 
-import br.com.letscode.starwarsnetwork.adapters.repositories.jpa.ParserEntidade;
-
 public interface ParserDTO<T> {
     T dtoDominio();
     ParserDTO<T> dominioDto(T dominio);
